f7eb3e2367061770cb2657845da7e484762860e2
brbarnett <barnett@echosolutionsgroup.com>
2016-03-25 15:34:50 -0500

    Previous close line

--- stockchart.js
+++ stockchart.js
@@ -1,20 +1,34 @@
-(function(){
+(function() {
     'use strict';
-    
+
     // extend for specific chart instance
-    var stockChart = $.extend(true, {}, 
+    var stockChart = $.extend(true, {},
         window.chart, {
-        chart: {
-            type: 'line'
-        },
-        series: [{
-            name: 'GOOGL',
-            data: GOOGL
-        }, {
-                name: 'MSFT',
-                data: MSFT
-            }]
-    });
-    
+            chart: {
+                type: 'line'
+            },
+            series: [{
+                name: 'GOOGL',
+                data: GOOGL
+            }, {
+                    name: 'MSFT',
+                    data: MSFT
+                }],
+            yAxis: {
+                plotLines: [{
+                    color: '#FFF',
+                    dashStyle: 'Dash',
+                    value: 200,
+                    width: 1,
+                    label: {
+                        text: 'Previous close',
+                        align: 'right',
+                        y: 12,
+                        x: 0
+                    }
+                }],
+            }
+        });
+
     $('#stockchart').highcharts('StockChart', stockChart);
-}());
+} ());
